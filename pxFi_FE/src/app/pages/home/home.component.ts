bffc8802a821aed64ec9d3e4c580b0d938f6c027
R-AlvesDev <ricardo.mr.alves@gmail.com>
2025-09-09 00:15:32 +0100

    Home component fix, working on live deploy

--- pxFi_FE/src/app/pages/home/home.component.ts
+++ pxFi_FE/src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { Observable, take, tap } from 'rxjs';
 import { AuthService } from '../../services/auth/auth.service';
 import { BankConnectionComponent } from '../bank-connection/bank-connection.component';
 import { DashboardComponent } from '../dashboard/dashboard.component'; // Import DashboardComponent
@@ -9,14 +9,26 @@ import { DashboardComponent } from '../dashboard/dashboard.component'; // Import
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AsyncPipe, RouterLink, BankConnectionComponent, DashboardComponent], // Add DashboardComponent
+  imports: [NgIf, AsyncPipe, RouterLink], 
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   isLoggedIn$: Observable<boolean>;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.isLoggedIn$ = this.authService.loggedIn$;
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn$.pipe(
+      take(1), // Take the first value to prevent multiple redirects
+      tap(isLoggedIn => {
+        if (isLoggedIn) {
+          // If the user is logged in, redirect them immediately to their accounts page
+          this.router.navigate(['/accounts']);
+        }
+      })
+    ).subscribe();
   }
 }
