6a1aad504b3f065d779e0423ee46990c8078200a
R-AlvesDev <ricardo.mr.alves@gmail.com>
2025-09-08 16:58:52 +0100

    Live deploy adjustments

--- pxFi_FE/src/app/services/api.service.ts
+++ pxFi_FE/src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 // ... (All interfaces remain the same) ...
 
@@ -164,7 +165,7 @@ export interface DashboardSummary {
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:8080/api';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
